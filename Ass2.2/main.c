130018f2b7ea19cc80921c6a1c1e6a589e78f2a3
Enmo7 <114494116+Enmo7@users.noreply.github.com>
2023-02-24 12:33:11 -0800

    Add files via upload

---
+++ Ass2.2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n,r,sum=0,temp;
+   printf("enter the number=\n");
+   scanf("%d",&n);
+   temp=n;
+   while(n>0)
+   {
+     r=n%10;
+     sum=sum+(r*r*r);
+    n=n/10;
+   }
+   if(temp==sum)
+   printf("armstrong  number\n ");
+   else
+   printf("not armstrong number\n");
+   return 0;
+}
